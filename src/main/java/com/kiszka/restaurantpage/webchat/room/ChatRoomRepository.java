b609aa67d980b0a27d02c044dcf521ff8c127d5e
Hubert <kiszkahub@gmail.com>
2024-04-14 16:44:07 +0200

    final version

--- src/main/java/com/kiszka/restaurantpage/webchat/room/ChatRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kiszka.restaurantpage.webchat.room;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.Optional;
-
-public interface ChatRoomRepository extends MongoRepository<ChatRoom,String> {
-    Optional<ChatRoom> findBySenderId(String senderId);
-}
